8c0a191d4593877d8478704041450fc15e444649
Frank Oh <fraoh@yahoo.com>
2017-11-06 12:39:27 -0800

    Readme

--- photobooth.py
+++ photobooth.py
@@ -11,13 +11,13 @@ from time import sleep
 
 # Variables
 BUTTON_LED = 20
-SWITCH = 21
+SWITCH = 26
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(SWITCH, GPIO.IN)
+GPIO.setup(SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON_LED, GPIO.OUT)
-GPIO.output(BUTTON_LED, True)
+GPIO.output(BUTTON_LED, 0)
 
 # Helper functions ###
 def get_filename_for_image():
@@ -53,29 +53,26 @@ def main():
     # Start Program
     print("Welcome to the photo booth!")
     print("Press the button to take a photo")
-
     # Wait for someone to push the button
     while True:
-        GPIO.output(BUTTON_LED, True)
-        if (GPIO.input(SWITCH)):
+        GPIO.output(BUTTON_LED, 1)
+        if (GPIO.input(SWITCH)== False):
             snap = 0
             while snap < 4:
                 print("pose!")
-                GPIO.output(BUTTON_LED, False)
-                #GPIO.output(POSE_LED, True)
+                GPIO.output(BUTTON_LED, 0)
                 time.sleep(1.5)
                 for i in range(5):
-                    #GPIO.output(POSE_LED, False)
+                    GPIO.output(BUTTON_LED, 1)
                     time.sleep(0.4)
-                    #GPIO.output(POSE_LED, True)
+                    GPIO.output(BUTTON_LED, 0)
                     time.sleep(0.4)
                 for i in range(5):
-                    #GPIO.output(POSE_LED, False)
+                    GPIO.output(BUTTON_LED, 1)
                     time.sleep(0.1)
-                    #GPIO.output(POSE_LED, True)
+                    GPIO.output(BUTTON_LED, 0)
                     time.sleep(0.1)
-                #GPIO.output(POSE_LED, False)
-                print("SNAP")
+                print("CHEESE")
                 gpout = subprocess.check_output(
                     "gphoto2 --capture-image-and-download --filename /home/pi/process_pix/%H%M%S.jpg",
                     stderr=subprocess.STDOUT, shell=True)
@@ -89,9 +86,8 @@ def main():
             # build image and send to printer
             subprocess.call("sudo /home/pi/scripts/process_image", shell=True)
             time.sleep(5)
-            print("ready for next round")
-            #GPIO.output(PRINT_LED, False)
-            GPIO.output(BUTTON_LED, True)
+            print("Press the button to take a photo!")
+            GPIO.output(BUTTON_LED, 1)
 if __name__ == "__main__":
     try:
         main()
